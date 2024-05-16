4f08460924c92d329d797245a12bc9af79b708e9
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2024-05-16 23:22:28 +0300

    Added async

--- IAnyBase.cs
+++ IAnyBase.cs
@@ -1,4 +1,5 @@
 ﻿using AnyBaseLib.Bases;
+using System.Data.Common;
 using System.Text.RegularExpressions;
 
 namespace AnyBaseLib;
@@ -7,5 +8,10 @@ public interface IAnyBase
 {
     public void Set(CommitMode commit_mode, string db_name, string db_host = "", string db_user = "", string db_pass = "");
     public List<List<string>> Query(string q, List<string> args = null, bool non_query = false);
+    public void QueryAsync(string q, List<string> args = null, Action<List<List<string>>> action = null, bool non_query = false);
+    //public void QueryDapper(Type type, string q, List<string> args = null);
+    //public void QueryDapperAsync(Type type, string q, List<string> args = null, Action<object> action = null);
+    public DbConnection GetConn();
+
     public bool Init();
 }
